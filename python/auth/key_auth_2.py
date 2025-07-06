521e1aebcbfae814ef64a6cd5de311b818d02d86
Otto Quaglio <otto.q@gmx.com>
2025-07-06 10:58:44 +0800

    refactor: ruff

--- python/auth/key_auth_2.py
+++ python/auth/key_auth_2.py
@@ -1,12 +1,16 @@
-import os
-import boto3
-import json
 import argparse
+import os
+
+import boto3
+
+# Using temporary credentials (e.g. from AWS STS or environment variables)
 
 parser = argparse.ArgumentParser(description="Get secrets from AWS Secrets Manager.")
 
 parser.add_argument("--region", required=True, help="AWS region")
-parser.add_argument("--secret-id", required=True, help="Secret ID in AWS Secrets Manager")
+parser.add_argument(
+    "--secret-id", required=True, help="Secret ID in AWS Secrets Manager"
+)
 args = parser.parse_args()
 
 print("Access Key:", os.environ.get("AWS_ACCESS_KEY_ID"))
@@ -16,7 +20,9 @@ print("Session Token:", os.environ.get("AWS_SESSION_TOKEN"))
 session = boto3.Session(
     aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
-    aws_session_token=os.environ.get("AWS_SESSION_TOKEN"),  # Required for temporary credentials
+    aws_session_token=os.environ.get(
+        "AWS_SESSION_TOKEN"
+    ),  # Required for temporary credentials
 )
 
 client = session.client("secretsmanager", region_name=args.region)
